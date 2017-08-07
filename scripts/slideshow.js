e81e682522fce768fcbfb54745e3197806a5882d
Daanyaal Syed <dsyed@jpl.nasa.gov>
2017-08-07 04:13:15 -0700

    Switch media

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -3,31 +3,52 @@ var interval = 1500;
 
 const $=document.querySelector.bind(document);
 
-let images = [
-    'images/IMG_0670.JPG',
-    'images/puppy.gif',
-    'images/tiger_cub.jpg'
-];
+var img = $('img');
+var video = $('video');
+
+// let images = [
+//     'images/IMG_0670.JPG',
+//     'images/puppy.gif',
+//     'images/tiger_cub.jpg'
+// ];
 
 let videos = [
     'videos/spider-man.mov',
     'videos/star-wars.mov'
 ];
 
+let media = {
+    'a': [
+        'spider-man.mov',
+        'tiger-cub.jpg'
+    ],
+    'b': [
+        'puppy.gif',
+        'spider-man.mov',
+        'star-wars.mov'
+    ]
+};
+
 var i = 0;
 
-function switchImage() {
-    let img = $('img');
-    img.src = images[i++];
-    i = i >= images.length ? 0 : i;
+function switchMedia() {
+    let url = videos[i];
+    i = i >= videos.length - 1 ? 0 : i + 1;
+
+    fetch(url)
+        .then(resp => resp.headers.get('Content-Type'))
+        .then(type => video.src = url);
 }
 
-function switchVideo() {
-    let video = $('video');
-    i = i >= videos.length - 1 ? 0 : i + 1;
-    video.src = videos[i];
-}
+// function switchImage() {
+//     img.src = images[i++];
+//     i = i >= images.length ? 0 : i;
+// }
+
+// function switchVideo() {
+//     i = i >= videos.length - 1 ? 0 : i + 1;
+//     video.src = videos[i];
+// }
 
 // setInterval(switchImage, interval);
-let video = $('video');
-video.onended = switchVideo;
+switchMedia();
