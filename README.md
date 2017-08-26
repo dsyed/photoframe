11785f861ddf79097c825775ba439aded34cc6af
Daanyaal Syed <dsyed@jpl.nasa.gov>
2017-08-26 13:30:58 -0700

    Add conf to disable screen blanking

---
+++ README.md
@@ -0,0 +1,9 @@
+# RPi Photoframe
+
+In the home directory (`~` or `/home/pi`):
+
+```bash
+wget -O install.sh https://raw.githubusercontent.com/dsyed/photoframe/master/install.sh
+chmod +x install.sh
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-git pull http://www.github.com/dsyed/photoframe
 curl -sSL https://get.docker.com | sh
 sudo apt-get install chromium
-cp photoframe/K99_shutdown /etc/rc6.d
+
+git pull http://www.github.com/dsyed/photoframe
+
+sudo cp photoframe/K99_shutdown /etc/rc6.d
 sudo chmod +x /etc/rc6.d/K99_shutdown
-cp autostart /etc/xdg/lxsession/LXDE/autostart
+
+sudo cp autostart /etc/xdg/lxsession/LXDE/autostart
+
+sudo cp lightdm.conf /etc/lightdm/lightdm.conf
